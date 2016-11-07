720299313a5ba9a73fe769b7b9780d868bee8293
Your Name <you@example.com>
2016-11-07 12:21:59 +0900

    session , redirect

--- moodcalendar/views.py
+++ moodcalendar/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from django.shortcuts import redirect
 
 
 
 def mycalendar(request):
+    if not request.user.is_authenticated():
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     return render(request, 'calendar/basic.html', {})
--- mysite/settings.py
+++ mysite/settings.py
@@ -128,6 +128,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_COOKIE_AGE = 1200
+SESSION_SAVE_EVERY_REQUEST = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
